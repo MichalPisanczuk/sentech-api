f3326276cf9972bf3c1224b54809c93df7032b99
Michał Pisańczuk <michal@pisanczuk.com>
2022-02-19 12:33:28 +0100

    Add port 8080

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const jsonServer = require("json-server");
 const server = json.server.create();
 const router = json.router("api.json");
 const midlewares = jsonServer.defaults();
-const port = process.env.PORT;
+const port = process.env.PORT || 8080;
 
 server.use(midlewares);
 server.use(router);
