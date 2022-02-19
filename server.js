9d506ef878681ee4efdb8655690236dfcb08200b
Michał Pisańczuk <michal@pisanczuk.com>
2022-02-19 12:29:28 +0100

    Delete port 3000

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const jsonServer = require("json-server");
 const server = json.server.create();
 const router = json.router("api.json");
 const midlewares = jsonServer.defaults();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT;
 
 server.use(midlewares);
 server.use(router);
